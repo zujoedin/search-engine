423afd2b25de1862950a1a3408cf9347cd08bf82
Edin_Zujo_CLS <edin.zujo@codelinesolutions.com>
2022-01-18 09:15:42 +0100

    push

--- resources/js/plugins/axios.js
+++ resources/js/plugins/axios.js
@@ -16,7 +16,7 @@ axios.interceptors.request.use(request => {
     request.headers.common['Accept-Language'] = locale
   }
 
-  request.headers['X-Socket-Id'] = Echo.socketId()
+  //request.headers['X-Socket-Id'] = Echo.socketId()
 
   return request
 })
