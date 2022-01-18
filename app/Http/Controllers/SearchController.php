ea6f3184ec046f685ee8a1b5aa4c04fdeab21e3b
Edin_Zujo_CLS <edin.zujo@codelinesolutions.com>
2022-01-18 12:05:43 +0100

    fix pg

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -108,12 +108,12 @@ class SearchController extends Controller
             if($search!=''){
                 $query->where(function($query) use ($search){                    
                     $query
-                        ->orWhere('contents.description', 'LIKE', "'%'.$search.'%'")
-                        ->orWhere('contents.title', 'LIKE', "'%'.$search.'%'")
+                        ->orWhere('contents.description', 'LIKE', "'%'.$search.'%'");
+                        // ->orWhere('contents.title', 'LIKE', "'%'.$search.'%'")
                         //In the taks the result should work with textual attributes, not dates, however I have added the year as well              
-                        ->orWhereYear('contents.release_date', '=', $search)
-                        ->orWhere('actors.last_name', 'LIKE', "'%'.$search.'%'")
-                        ->orWhere('actors.first_name', 'LIKE', "'%'.$search.'%'");
+                        // ->orWhereYear('contents.release_date', '=', $search)
+                        // ->orWhere('actors.last_name', 'LIKE', "'%'.$search.'%'")
+                        // ->orWhere('actors.first_name', 'LIKE', "'%'.$search.'%'");
                 });
             }
             if($add_search_phrase){
